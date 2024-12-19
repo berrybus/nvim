a824b66d4059ad27a880b8b6a74d35de25b28d61
Nancy Zhang <nancy@redbeancat.com>
2024-12-18 22:15:41 -0500

    add extension

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   -- disable trouble
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  --  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  -- { "echasnovski/mini.pairs", enabled = false },
 }
