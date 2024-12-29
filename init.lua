655f9af2a6625af059144e469fca4ad2df8523ea
Nancy Zhang <nancy.zhang143@gmail.com>
2024-12-29 18:33:46 -0500

    disable placeholder arg

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup({
+  cmd = { "clangd", "--function-arg-placeholders=0" },
+})
